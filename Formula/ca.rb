6eac9021feaf3f6b96c8ef73b9db09411beb8660
onevcat <onevcat@gmail.com>
2026-01-04 00:22:02 +0900

    Add ca formula and automation

---
+++ Formula/ca.rb
@@ -0,0 +1,24 @@
+class Ca < Formula
+  desc "Chroma-powered cat replacement with syntax highlighting"
+  homepage "https://github.com/onevcat/Chroma"
+  url "https://github.com/onevcat/Chroma/archive/refs/tags/0.1.0.tar.gz"
+  sha256 "cfc26d8c66b341594440798228dc2b13dfc64f884b61751ba34b0b973288f731"
+  license "MIT"
+
+  on_macos do
+    depends_on macos: :sequoia
+  end
+
+  on_linux do
+    depends_on "swift" => :build
+  end
+
+  def install
+    system "swift", "build", "-c", "release", "--disable-sandbox"
+    bin.install ".build/release/ca"
+  end
+
+  test do
+    assert_match "ca", shell_output("#{bin}/ca --help")
+  end
+end
---
+++ README.md
@@ -0,0 +1,14 @@
+# homebrew-tap
+
+Homebrew tap for onevcat tools.
+
+## Install ca
+
+```bash
+brew install onevcat/tap/ca
+```
+
+## Bottles
+
+Bottles are built and published automatically after a Chroma release for supported platforms.
+If a bottle is not available, Homebrew will build from source.
